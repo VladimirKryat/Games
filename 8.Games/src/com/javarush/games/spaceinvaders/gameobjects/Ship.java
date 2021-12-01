4b858dc68670694a26830acca41f4ffd984a4830
UbVova <ubvova@local.tomcat.ru>
2021-12-02 01:17:28 +0300

    Add animation for ship

--- 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Ship.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Ship.java
@@ -1,11 +1,24 @@
 package com.javarush.games.spaceinvaders.gameobjects;
 
+import com.javarush.engine.cell.Game;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Ship extends GameObject{
+
+    //список матриц для кадров анимации
+    private List<int[][]> frames;
+    //текущий кадр анимации
+    private int frameIndex;
     public boolean isAlive=true;
     public Ship(double x, double y) {
         super(x, y);
     }
     public void setStaticView(int [][] viewFrame){
+        frames=new ArrayList<>();
+        frames.add(viewFrame);
+        frameIndex=0;
         setMatrix(viewFrame);
     }
     public Bullet fire(){
@@ -13,5 +26,23 @@ public class Ship extends GameObject{
     };
     public void kill(){
         isAlive=false;
+    }
+    public void setAnimatedView(int[][]... viewFrames){
+        setMatrix(viewFrames[0]);
+        frameIndex=0;
+        frames=Arrays.asList(viewFrames);
+    }
+//    устанавливаем следущий кадр, если не установлен последний кадр
+    public void nextFrame(){
+        if (frameIndex<frames.size()-1){
+            frameIndex++;
+            setMatrix(frames.get(frameIndex));
+        }
+    }
+
+    @Override
+    public void draw(Game game) {
+        nextFrame();
+        super.draw(game);
     }
 }
